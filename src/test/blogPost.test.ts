84040838499b4f987f607f50e142b69e3abf2541
selbin <selbinzr@gmail.com>
2023-09-05 20:40:38 +0530

    corrected testfile

--- src/test/blogPost.test.ts
+++ src/test/blogPost.test.ts
@@ -1,10 +1,10 @@
 import request from 'supertest';
 
-import { appInstance } from '../index';
+import app from '../index';
 
 describe('GET /api/posts', () => {
     it('should return an array of blog posts ', async () => {
-        const response = await request(appInstance()).get('/api/posts');
+        const response = await request(app).get('/api/posts');
         expect(response.status).toBe(200);
     });
 });
